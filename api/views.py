6415d69b0683fc27a9805cf24b138c56fc171c6a
gurpreet-5555 <63334405+gurpreet-5555@users.noreply.github.com>
2025-03-25 12:15:41 -0230

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+#views.py
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response 
