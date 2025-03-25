fab8c23420c8d0a31d0a55911cfc14d7020c7079
gurpreet-5555 <63334405+gurpreet-5555@users.noreply.github.com>
2025-03-25 12:00:30 -0230

    Update settings.py

--- ColabDev/settings.py
+++ ColabDev/settings.py
@@ -198,7 +198,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER = 'sayyamkundra2@gmail.com'
-EMAIL_HOST_PASSWORD ='qjei bzxp zfdh froq'
+EMAIL_HOST_PASSWORD ='xmgw ytbx dsum toli'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
